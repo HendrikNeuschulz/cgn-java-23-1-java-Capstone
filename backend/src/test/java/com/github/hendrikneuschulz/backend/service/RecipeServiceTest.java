92dac29cb1118c779e1dd5dde7c4b6b309f007b8
Hendrik Neuschulz <hneuhb@t-online.de>
2023-03-27 16:00:13 +0200

    implement Test for getRandomRecipe

--- backend/src/test/java/com/github/hendrikneuschulz/backend/service/RecipeServiceTest.java
+++ backend/src/test/java/com/github/hendrikneuschulz/backend/service/RecipeServiceTest.java
@@ -3,21 +3,23 @@ package com.github.hendrikneuschulz.backend.service;
 import com.github.hendrikneuschulz.backend.model.Recipe;
 import com.github.hendrikneuschulz.backend.repository.RecipeRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
- class RecipeServiceTest {
+class RecipeServiceTest {
     @Test
     void testGetRecipeList() {
 
-        RecipeRepository recipeRepository = Mockito.mock(RecipeRepository.class);
+        RecipeRepository recipeRepository = mock(RecipeRepository.class);
         List<Recipe> mockRecipes = new ArrayList<>();
         mockRecipes.add(new Recipe("Recipe 1"));
         mockRecipes.add(new Recipe("Recipe 2"));
-        Mockito.when(recipeRepository.findAll()).thenReturn(mockRecipes);
+        when(recipeRepository.findAll()).thenReturn(mockRecipes);
 
 
         RecipeService recipeService = new RecipeService(recipeRepository);
@@ -27,4 +29,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         assertEquals("Recipe 1", recipeList.get(0).getName());
         assertEquals("Recipe 2", recipeList.get(1).getName());
     }
+
+
 }
