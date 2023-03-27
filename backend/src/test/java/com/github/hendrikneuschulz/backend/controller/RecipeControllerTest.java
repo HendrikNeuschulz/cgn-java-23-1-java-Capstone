95eb10b180ae83a68317f05859f03efe17544c6f
Hendrik Neuschulz <hneuhb@t-online.de>
2023-03-27 20:35:46 +0200

    fix tests

--- backend/src/test/java/com/github/hendrikneuschulz/backend/controller/RecipeControllerTest.java
+++ backend/src/test/java/com/github/hendrikneuschulz/backend/controller/RecipeControllerTest.java
@@ -9,8 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
-
 
 class RecipeControllerTest {
     private RecipeService recipeServiceMock;
@@ -23,8 +23,7 @@ class RecipeControllerTest {
     }
 
     @Test
-     void testGetRecipes() {
-
+    void testGetRecipes() {
         Recipe recipe1 = new Recipe("Recipe 1");
         Recipe recipe2 = new Recipe("Recipe 2");
 
@@ -33,12 +32,21 @@ class RecipeControllerTest {
         recipeList.add(recipe2);
         when(recipeServiceMock.getRecipeList()).thenReturn(recipeList);
 
-
         List<Recipe> result = recipeController.getRecipes();
-
 
         assertEquals(recipeList, result);
         verify(recipeServiceMock, times(1)).getRecipeList();
     }
 
+    @Test
+    void testGetRandomRecipe() {
+        Recipe randomRecipe = new Recipe("Random Recipe");
+        when(recipeServiceMock.getRandomRecipe()).thenReturn(randomRecipe);
+
+        Recipe result = recipeController.getRandomRecipe();
+
+        assertNotNull(result);
+        assertEquals(randomRecipe.getName(), result.getName());
+        verify(recipeServiceMock, times(1)).getRandomRecipe();
+    }
 }
