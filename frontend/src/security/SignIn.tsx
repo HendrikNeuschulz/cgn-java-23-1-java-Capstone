5f0744351f1b416e6ab149998c06d450e1f235f3
Hendrik Neuschulz <hneuhb@t-online.de>
2023-04-13 16:16:01 +0200

    create new Hook for sign in & sign up

--- frontend/src/security/SignIn.tsx
+++ frontend/src/security/SignIn.tsx
@@ -1,37 +1,19 @@
-import React from "react";
-import {useNavigate} from "react-router-dom";
-import axios from "axios";
+import {useAuth} from "../Hooks/useSignInSignUp";
 
 export default function SignIn() {
-    const [username, setUsername] = React.useState<string>("");
-    const [password, setPassword] = React.useState<string>("");
-    const navigate = useNavigate();
-
+    const {signIn: {username, password, handleUsernameChange, handlePasswordChange, handleSubmit}} = useAuth();
     return (
         <div style={{padding: "5rem 0"}}>
             <h1>Sign In</h1>
 
-            <form onSubmit={e => {
-                e.preventDefault();
-                axios.post("/api/users/login", {}, {
-                    headers: {
-                        Authorization: `Basic ${window.btoa(`${username}:${password}`)}`
-                    }
-                }).then(() => {
-                    const redirect = window.sessionStorage.getItem("signInRedirect") || "/";
-                    window.sessionStorage.removeItem("signInRedirect");
-                    navigate(redirect);
-                }).catch(err => {
-                    alert(err.response.data.error);
-                });
-            }}>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label>
                         Username
                         <input
                             type="text"
                             value={username}
-                            onChange={e => setUsername(e.currentTarget.value)}
+                            onChange={handleUsernameChange}
                         />
                     </label>
                 </div>
@@ -42,7 +24,7 @@ export default function SignIn() {
                         <input
                             type="password"
                             value={password}
-                            onChange={e => setPassword(e.currentTarget.value)}
+                            onChange={handlePasswordChange}
                         />
                     </label>
                 </div>
--- frontend/src/security/SignUp.tsx
+++ frontend/src/security/SignUp.tsx
@@ -1,34 +1,21 @@
 import React from "react";
-import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import {useAuth} from "../Hooks/useSignInSignUp";
 
 export default function SignUp() {
-    const [username, setUsername] = React.useState<string>("");
-    const [password, setPassword] = React.useState<string>("");
-    const navigate = useNavigate();
+    const {signUp: {username, password, handleUsernameChange, handlePasswordChange, handleSubmit}} = useAuth();
 
     return (
         <div style={{padding: "5rem 0"}}>
             <h1>Sign Up</h1>
 
-            <form onSubmit={e => {
-                e.preventDefault();
-                axios.post("/api/users", {
-                    username,
-                    password
-                }).then(() => {
-                    navigate("/sign-in");
-                }).catch(err => {
-                    alert(err.response.data.error);
-                });
-            }}>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label>
                         Username
                         <input
                             type="text"
                             value={username}
-                            onChange={e => setUsername(e.currentTarget.value)}
+                            onChange={handleUsernameChange}
                         />
                     </label>
                 </div>
@@ -39,7 +26,7 @@ export default function SignUp() {
                         <input
                             type="password"
                             value={password}
-                            onChange={e => setPassword(e.currentTarget.value)}
+                            onChange={handlePasswordChange}
                         />
                     </label>
                 </div>
