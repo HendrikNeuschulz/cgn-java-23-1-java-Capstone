e509d2fead5da58f41cff6455c3d9e5e7e288981
Hendrik Neuschulz <hneuhb@t-online.de>
2023-04-13 16:36:27 +0200

    fix some sonar issues

--- frontend/src/security/LogOut.tsx
+++ frontend/src/security/LogOut.tsx
@@ -1,9 +1,7 @@
 import axios from "axios";
-import useAuth from "../Hooks/useAuth";
 import {useLocation} from "react-router-dom";
 
 export default function LogOut() {
-    const user = useAuth(false);
     const location = useLocation();
 
     axios.post("/api/users/logout").then(() => {
